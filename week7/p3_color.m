8f986ab3e604f9cf06dae0b5e6ff088495207fa6
chian-chen <64936325+chian-chen@users.noreply.github.com>
2022-09-14 14:05:44 +0800

    finish week9 Corner Detection code

---
+++ week7/p3_color.m
@@ -0,0 +1,58 @@
+% img x
+
+x = double(imread('./a.jpg'));
+figure, imshow(x/255);
+
+
+% m, n, d
+
+[m, n, d] = size(x);
+
+% generate k
+
+k = zeros(21, 21);
+
+for i = 1:21
+    for j = 1:21
+        k(i, j) = exp(-0.1 * ((i - 11)^2 + (j - 11)^2));
+    end
+end
+
+s = sum(sum(k));
+k = k/s;
+
+% noise
+
+noise = (rand(m, n) - 0.5) * 0.25;
+
+% y
+
+y = convn(x, k, 'same') + noise;
+
+figure, imshow(y/255);
+
+% k1
+
+k1 = zeros(m, n);
+
+k1(1:11, 1:11) = k(11:21, 11:21);
+k1(1:11, n - 9 : n) = k(11:21, 1:10);
+k1(m - 9 : m, 1:11) = k(1:10, 11:21);
+k1(m - 9 : m, n - 9 : n) = k(1:10, 1:10);
+
+K = fft2(k1);
+
+% generate H from K
+
+c = 0.01;
+H = 1 ./ (c./conj(K) + K);
+
+% recovery
+
+x_new = ifft2(fft2(y) .* H);
+
+figure, imshow(x_new/255);
+
+
+
+
---
+++ week9/CornerDetection.m
@@ -0,0 +1,84 @@
+% im
+
+I = double(imread('./RnzgH.jpg'));
+I = rgb2gray(I/255);
+
+figure, imshow(I);
+
+
+% X, Y
+
+X = conv2(I, [-1, 0, 1], 'same');
+Y = conv2(I, [-1, 0, 1]' , 'same');
+
+figure, imshow(X);
+figure, imshow(Y);
+
+
+% m, n
+
+[m, n] = size(I);
+
+% w use built-in function 
+% ref : https://es.mathworks.com/help/images/ref/fspecial.html
+
+w = fspecial('gaussian', [30 30], 4);
+w = w .* 2 .* pi .* 16;
+surf(w)
+
+% A, B
+
+A = conv2(X.^2, w, 'same');
+B = conv2(Y.^2, w, 'same');
+C = conv2(X.*Y, w, 'same');
+
+figure, imshow(A);
+figure, imshow(B);
+figure, imshow(C);
+
+
+% R
+
+k = 0.04;
+R = A .* B - C.^2 - k * (A + B).^2;
+threshold = max(max(R))/100;
+
+
+% corner
+
+corner = zeros(m, n);
+
+% ignore the edge of image
+for i = 2 : m - 1
+    for j = 2 : n - 1
+        Region = R(i - 1:i + 1, j - 1:j + 1);
+        local_max = max(max(Region));
+        if R(i, j) > threshold && R(i, j) == local_max
+            corner(i, j) = 1;
+        end
+    end
+end
+
+figure, imshow(corner);
+
+% Dilation 3 times
+corner = Dilation(Dilation(Dilation(corner)));
+figure, imshow(corner);
+
+
+% result
+
+figure, imshow(I + corner);
+
+
+function B = Dilation(M)
+    M_new = M;
+    [row,col] = size(M);
+    for i = 2:row - 1
+        for j =2:col - 1
+            M_new(i, j) = max([M(i, j), M(i + 1, j), M(i - 1, j), M(i, j + 1), M(i, j - 1)]);
+        end
+    end
+    B = M_new;
+end
+
